d27edc37913b1142a404ac1d6b2de64b0c7ec3ce
Tab Atkins-Bittner <jackalmage@gmail.com>
2021-10-06 18:45:41 -0700

    black

--- kdl/__init__.py
+++ kdl/__init__.py
@@ -2,4 +2,15 @@ from . import printing
 
 from .parsefuncs import parse
 from .errors import ParseError
-from .types import Document, Node, Binary, Octal, Decimal, Hex, Bool, Null, RawString, String
+from .types import (
+    Document,
+    Node,
+    Binary,
+    Octal,
+    Decimal,
+    Hex,
+    Bool,
+    Null,
+    RawString,
+    String,
+)
